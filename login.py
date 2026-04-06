60f720a5b0cb1a5c5d2bd570784fa7be5744b0b2
mandwalprakash <mandwal.prakash@gmail.com>
2026-04-06 12:43:51 +0530

    Added file on repo

---
+++ login.py
@@ -0,0 +1,12 @@
+# Taking user input
+name = input("Enter your Name: ")
+surname = input("Enter your Surname: ")
+email = input("Enter your Email Address: ")
+phone = input("Enter your Phone Number: ")
+
+# Displaying the entered data
+print("\n--- Your Details ---")
+print("Name:", name)
+print("Surname:", surname)
+print("Email:", email)
+print("Phone Number:", phone)
